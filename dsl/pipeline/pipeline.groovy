242e6b0fb4dd6af436877d0baab414bf36e70a16
Stephen Kuenzli <skuenzli@qualimente.com>
2016-04-19 05:43:52 -0700

    Give pipeline a better name for demoing, resolve import, specify package.

--- dsl/pipeline/pipeline.groovy
+++ dsl/pipeline/pipeline.groovy
@@ -1,12 +1,17 @@
-deliveryPipelineView('Dummy Pipeline Example') {
+package pipeline
+
+import javaposse.jobdsl.dsl.views.DeliveryPipelineView
+
+def pipelineName = 'Product Pipeline Example'
+deliveryPipelineView(pipelineName) {
   pipelineInstances(5)
     columns(1)
-    sorting(Sorting.LAST_ACTIVITY)
+    sorting(DeliveryPipelineView.Sorting.LAST_ACTIVITY)
     updateInterval(10)
     enableManualTriggers(true)
     showAvatars()
     showChangeLog()
     pipelines {
-      component('Dummy Pipeline Example', 'unit_test')
+      component(pipelineName, 'unit_test')
     }
 }
