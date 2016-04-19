1a3ca4d5d723bb0b598c197163a6a587fdc912d2
Stephen Kuenzli <skuenzli@qualimente.com>
2016-04-19 06:27:00 -0700

    Add a 'Test' phase to the simple delivery pipeline.

--- dsl/pipeline/jobs/simple_delivery_pipeline_jobs.groovy
+++ dsl/pipeline/jobs/simple_delivery_pipeline_jobs.groovy
@@ -11,6 +11,9 @@ def jobNames = [
     compile_and_unit_test: "${productName}-compile_and_unit_test"
     , integration_test : "${productName}-integration_test"
     , sonar_quality_gate : "${productName}-sonar_quality_gate"
+    , deploy_to_stage : "${productName}-deploy_to_stage"
+    , api_tests_stage : "${productName}-api_tests_stage"
+    , e2e_tests_stage : "${productName}-e2e_tests_stage"
 ]
 
 Job compile_and_unit_test = JobHelper.createJob(this as DslFactory, jobNames.compile_and_unit_test)
@@ -26,6 +29,19 @@ JobHelper.addDownstreamParameterized(integration_test, [jobNames.sonar_quality_g
 Job sonar_quality_gate = JobHelper.createJob(this as DslFactory, jobNames.sonar_quality_gate)
 JobHelper.addStep(sonar_quality_gate, "sleep \$((RANDOM%10+5))")
 JobHelper.addDeliveryPipelineConfiguration(sonar_quality_gate, 'Build', 'Sonar Code Quality Gate')
+
+Job deploy_to_stage = JobHelper.createJob(this as DslFactory, jobNames.deploy_to_stage)
+JobHelper.addStep(deploy_to_stage, "sleep \$((RANDOM%10+5))")
+JobHelper.addDownstreamParameterized(deploy_to_stage, [jobNames.api_tests_stage, jobNames.e2e_tests_stage], "SUCCESS")
+JobHelper.addDeliveryPipelineConfiguration(deploy_to_stage, 'Test', 'Deploy to Stage')
+
+Job api_tests_stage = JobHelper.createJob(this as DslFactory, jobNames.api_tests_stage)
+JobHelper.addStep(api_tests_stage, "sleep \$((RANDOM%10+5))")
+JobHelper.addDeliveryPipelineConfiguration(api_tests_stage, 'Test', 'API Tests - Stage')
+
+Job e2e_tests_stage = JobHelper.createJob(this as DslFactory, jobNames.e2e_tests_stage)
+JobHelper.addStep(e2e_tests_stage, "sleep \$((RANDOM%10+5))")
+JobHelper.addDeliveryPipelineConfiguration(e2e_tests_stage, 'Test', 'e2e Functional Tests - Stage')
 
 def pipelineName = "${productName} Delivery Pipeline"
 deliveryPipelineView(pipelineName) {
